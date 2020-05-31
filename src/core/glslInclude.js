35f5226be78c0fc6bbe1fbb5d1559e21c9da7cb4
Yiwei Gong <imwithye@gmail.com>
2020-05-31 18:45:30 +0800

    use pixel coordinate

--- src/core/glslInclude.js
+++ src/core/glslInclude.js
@@ -8,42 +8,37 @@ uniform vec4 iPlot;
 
 float DistanceToLine(vec2 p0, vec2 p1, vec2 p)
 {
+    p0 = ((p0 - iPlot.xy) + iPlot.zw) / (iPlot.zw * 2.) * iResolution.xy;
+    p1 = ((p1 - iPlot.xy) + iPlot.zw) / (iPlot.zw * 2.) * iResolution.xy;
+    p = ((p - iPlot.xy) + iPlot.zw) / (iPlot.zw * 2.) * iResolution.xy;
     vec2 ld = p0 - p1;
     vec2 pd = p - p1;
-    return length(pd - ld*dot(pd, ld)/dot(ld, ld));    
+    return length(pd - ld*clamp(dot(pd, ld)/dot(ld, ld), 0.0, 1.0)); 
 }
 
 #define DistanceToFunc(Func, p, dist)                                             \\
 {                                                                                 \\
     float xDelta = iPlot.z * 2. / iResolution.x;                                  \\
-    float offset = xDelta * -3.;                                                  \\
+    float offset = xDelta * -2.;                                                  \\
     vec2 p0 = vec2(p.x + offset, Func(p.x + offset));                             \\
     vec2 p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));           \\
-    dist = DistanceToLine(p0, p1, p);                                             \\
-    offset = xDelta * -2.;                                                        \\
-    p0 = vec2(p.x + offset, Func(p.x + offset));                                  \\
-    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));                \\
     dist = DistanceToLine(p0, p1, p);                                             \\
     offset = xDelta * -1.;                                                        \\
     p0 = vec2(p.x + offset, Func(p.x + offset));                                  \\
     p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));                \\
-    dist = DistanceToLine(p0, p1, p);                                             \\
+    dist = min(dist, DistanceToLine(p0, p1, p));                                  \\
     offset = xDelta * 0.;                                                         \\
     p0 = vec2(p.x + offset, Func(p.x + offset));                                  \\
     p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));                \\
-    dist = DistanceToLine(p0, p1, p);                                             \\
+    dist = min(dist, DistanceToLine(p0, p1, p));                                  \\
     offset = xDelta * 1.;                                                         \\
     p0 = vec2(p.x + offset, Func(p.x + offset));                                  \\
     p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));                \\
-    dist = DistanceToLine(p0, p1, p);                                             \\
+    dist = min(dist, DistanceToLine(p0, p1, p));                                  \\
     offset = xDelta * 2.;                                                         \\
     p0 = vec2(p.x + offset, Func(p.x + offset));                                  \\
     p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));                \\
-    dist = DistanceToLine(p0, p1, p);                                             \\
-    offset = xDelta * 3.;                                                         \\
-    p0 = vec2(p.x + offset, Func(p.x + offset));                                  \\
-    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));                \\
-    dist = DistanceToLine(p0, p1, p);                                             \\
+    dist = min(dist, DistanceToLine(p0, p1, p));                                  \\
 }
 
 #define Plot(Func, fragCoord, col)                                                \\
@@ -51,13 +46,9 @@ float DistanceToLine(vec2 p0, vec2 p1, vec2 p)
     vec2 uv = fragCoord / iResolution.xy;                                         \\
     vec2 range = vec2(iPlot.z * 2., iPlot.w * 2.);                                \\
     vec2 p = uv * range - range / 2. + iPlot.xy;                                  \\
-    vec2 f = vec2(p.x, Func(p.x));                                                \\
-    vec2 pv = uv * iResolution.xy;                                                \\
-    vec2 fv = ((f - iPlot.xy) + range / 2.) / range * iResolution.xy;             \\
-    float distp = length(pv - fv);                                                \\
     float dist = 0.;                                                              \\
     DistanceToFunc(Func, p, dist);                                                \\
-    float intensity = smoothstep(0., 1., 1. - distp / 1.2);                       \\
+    float intensity = smoothstep(0., 1., 1. - dist);                              \\
     col *= pow(intensity, 1./2.2);                                                \\
 }
 `;
