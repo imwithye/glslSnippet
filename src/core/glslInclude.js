bcbabd7fa685337bbf6d7f70f88cb6dab79d3a21
Yiwei Gong <imwithye@gmail.com>
2020-05-31 16:48:18 +0800

    add plot function

--- src/core/glslInclude.js
+++ src/core/glslInclude.js
@@ -5,7 +5,7 @@ uniform float iTimeDelta;
 uniform float iFrame;
 uniform vec4 iMouse;
 
-vec4 iPlot = vec4(-10., 10., -10., 10.);
+vec4 iPlot = vec4(0., 0., 1, 1);
 
 float DistanceToLine(vec2 p0, vec2 p1, vec2 p)
 {
@@ -14,48 +14,51 @@ float DistanceToLine(vec2 p0, vec2 p1, vec2 p)
     return length(pd - ld*dot(pd, ld)/dot(ld, ld));    
 }
 
-#define DistanceToFunc(Func, p, dist)\
-{\
-    float xDelta = (iPlot.y - iPlot.x) / iResolution.x;\
-    float offset = xDelta * -3.;\
-    vec2 p0 = vec2(p.x + offset, Func(p.x + offset));\
-    vec2 p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));\
-    dist = DistanceToLine(p0, p1, p);\
-    offset = xDelta * -2.;\
-    p0 = vec2(p.x + offset, Func(p.x + offset));\
-    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));\
-    dist = DistanceToLine(p0, p1, p);\
-    offset = xDelta * -1.;\
-    p0 = vec2(p.x + offset, Func(p.x + offset));\
-    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));\
-    dist = DistanceToLine(p0, p1, p);\
-    offset = xDelta * 0.;\
-    p0 = vec2(p.x + offset, Func(p.x + offset));\
-    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));\
-    dist = DistanceToLine(p0, p1, p);\
-    offset = xDelta * 1.;\
-    p0 = vec2(p.x + offset, Func(p.x + offset));\
-    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));\
-    dist = DistanceToLine(p0, p1, p);\
-    offset = xDelta * 2.;\
-    p0 = vec2(p.x + offset, Func(p.x + offset));\
-    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));\
-    dist = DistanceToLine(p0, p1, p);\
-    offset = xDelta * 3.;\
-    p0 = vec2(p.x + offset, Func(p.x + offset));\
-    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));\
-    dist = DistanceToLine(p0, p1, p);\
-}\
+#define DistanceToFunc(Func, p, dist)                                             \\
+{                                                                                 \\
+    float xDelta = iPlot.z * 2. / iResolution.x;                                  \\
+    float offset = xDelta * -3.;                                                  \\
+    vec2 p0 = vec2(p.x + offset, Func(p.x + offset));                             \\
+    vec2 p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));           \\
+    dist = DistanceToLine(p0, p1, p);                                             \\
+    offset = xDelta * -2.;                                                        \\
+    p0 = vec2(p.x + offset, Func(p.x + offset));                                  \\
+    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));                \\
+    dist = DistanceToLine(p0, p1, p);                                             \\
+    offset = xDelta * -1.;                                                        \\
+    p0 = vec2(p.x + offset, Func(p.x + offset));                                  \\
+    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));                \\
+    dist = DistanceToLine(p0, p1, p);                                             \\
+    offset = xDelta * 0.;                                                         \\
+    p0 = vec2(p.x + offset, Func(p.x + offset));                                  \\
+    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));                \\
+    dist = DistanceToLine(p0, p1, p);                                             \\
+    offset = xDelta * 1.;                                                         \\
+    p0 = vec2(p.x + offset, Func(p.x + offset));                                  \\
+    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));                \\
+    dist = DistanceToLine(p0, p1, p);                                             \\
+    offset = xDelta * 2.;                                                         \\
+    p0 = vec2(p.x + offset, Func(p.x + offset));                                  \\
+    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));                \\
+    dist = DistanceToLine(p0, p1, p);                                             \\
+    offset = xDelta * 3.;                                                         \\
+    p0 = vec2(p.x + offset, Func(p.x + offset));                                  \\
+    p1 = vec2(p.x + offset + xDelta, Func(p.x + offset + xDelta));                \\
+    dist = DistanceToLine(p0, p1, p);                                             \\
+}
 
-#define Plot(Func, fragCoord, col)\
-{\
-    vec2 uv = fragCoord / iResolution.y;\
-    float xRange = iPlot.y - iPlot.x;\
-    float yRange = iPlot.w - iPlot.z;\
-    vec2 p = vec2(uv.x * xRange - xRange / 2., uv.y * yRange - yRange / 2.);\
-    float dist = 0.;\
-    DistanceToFunc(Func, p, dist);\
-    float intensity = smoothstep(0., 1., 1. - dist * 10.);\
-    col *= pow(intensity, 1./2.2);\
-}\
+#define Plot(Func, fragCoord, col)                                                \\
+{                                                                                 \\
+    vec2 uv = fragCoord / iResolution.xy;                                         \\
+    vec2 range = vec2(iPlot.z * 2., iPlot.w * 2.);                                \\
+    vec2 p = uv * range - range / 2. + iPlot.xy;                                  \\
+    vec2 f = vec2(p.x, Func(p.x));                                                \\
+    vec2 pv = uv * iResolution.xy;                                                \\
+    vec2 fv = ((f - iPlot.xy) + range / 2.) / range * iResolution.xy;             \\
+    float distp = length(pv - fv);                                                \\
+    float dist = 0.;                                                              \\
+    DistanceToFunc(Func, p, dist);                                                \\
+    float intensity = smoothstep(0., 1., 1. - distp / 1.2);                       \\
+    col *= pow(intensity, 1./2.2);                                                \\
+}
 `;
