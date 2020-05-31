a6bdb1c10941efc83f2f373b13ffb799118f60ff
Yiwei Gong <imwithye@gmail.com>
2020-05-31 15:07:10 +0800

    add distance to line segment

--- src/core/glslInclude.js
+++ src/core/glslInclude.js
@@ -4,4 +4,11 @@ uniform float iTime;
 uniform float iTimeDelta;
 uniform float iFrame;
 uniform vec4 iMouse;
+
+float glslSnippet_DistanceToLineSegment(vec2 p0, vec2 p1, vec2 p)
+{
+    vec2 ld = p0 - p1;
+    vec2 pd = p - p1;
+    return length(pd - ld*dot(pd, ld)/dot(ld, ld));    
+}
 `;
--- src/core/shader.js
+++ src/core/shader.js
@@ -6,34 +6,21 @@ const VertBuffer = {
 const VertCode = `
 #version 300 es
 layout(location = 0) in vec4 pos;
-void main() { 
-  gl_Position = pos;
-}`;
+void main() { gl_Position = pos; }`;
 
 const VertCodeGL1 = `
 attribute vec4 pos;
-
-void main() { 
-  gl_Position = pos;
-}`;
+void main() { gl_Position = pos; }`;
 
 const FragCodeError = `
 #version 300 es
-
 precision highp float;
-
 out vec4 FragColor;
-
-void main() {
-  FragColor = vec4(1, 0, 1, 1);
-}`;
+void main() { FragColor = vec4(1, 0, 1, 1); }`;
 
 const FragCodeErrorGL1 = `
 precision mediump float;
-
-void main() {
-  gl_FragColor = vec4(1, 0, 1, 1);
-}`;
+void main() { gl_FragColor = vec4(1, 0, 1, 1); }`;
 
 const FragCodeHeader = `
 #version 300 es
@@ -42,26 +29,21 @@ const FragCodeHeader = `
 
 precision highp float;
 out vec4 FragColor;
-
 ${glslInclude}
-
 `;
 
 const FragCodeHeaderGL1 = `
 #define WebGL1
 
 precision highp float;
-
 ${glslInclude}
-
 `;
 
 const FragCodeFooter = `
   
 void main() {
     mainImage(FragColor, gl_FragCoord.xy);
-}
-`;
+}`;
 
 const FragCodeFooterGL1 = `
 
@@ -70,8 +52,7 @@ void main() {
     vec2 fragCoord = gl_FragCoord.xy;
     mainImage(fragColor, fragCoord);
     gl_FragColor = fragColor;
-}
-`;
+}`;
 
 module.exports = {
   VertBuffer,
